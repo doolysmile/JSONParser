79c99b0a813bf0c767ee29c720c458ceb8e0dfbd
KCH <doolysmile1@naver.com>
2022-10-06 23:58:52 +0900

    global url 추가

--- src/main/java/src/Global.java
+++ src/main/java/src/Global.java
@@ -2,9 +2,15 @@ package src;
 
 public class Global {
 
-    public static final String BASE_RUL = "https://kox947ka1a.execute-api.ap-northeast-2.amazonaws.com/prod/users";
+    public static final String BASE_RUL = "https://huqeyhi95c.execute-api.ap-northeast-2.amazonaws.com/prod";
 
     public static final String POST_START = "/start";
-    public static final String X_AUTH_TOKEN = "9c3355168ff38001e92e48f63ef1fe4d";
+    public static final String GET_WAITING = "/waiting_line";
+    public static final String GET_RESULT = "/game_result";
+    public static final String GET_USERINFO = "/user_info";
+    public static final String PUT_MATCH = "/match";
+    public static final String PUT_CHANGE = "/change_grade";
+    public static final String GET_SCORE ="/score";
+    public static final String X_AUTH_TOKEN = "6981271666c6e99bf854206e0a1ff87c";
 
 }
